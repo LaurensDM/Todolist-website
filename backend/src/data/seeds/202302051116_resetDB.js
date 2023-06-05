3661dd37fed2524e439af7364a92106f59e899e3
LaurensDM <laurens.demaeyer@student.hogent.be>
2023-06-05 12:39:50 +0200

    Getting rid of seperate backend

--- backend/src/data/seeds/202302051116_resetDB.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  seed: async (knex) => {
-    // first delete all entries in every table
-    await knex('Item').delete();
-    await knex('List').delete();
-    await knex('Users').delete();   
-  },
-};
